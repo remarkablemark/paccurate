5225a1018169255ded6b18efed3767fc10f90b57
Mark <mark@remarkablemark.org>
2025-06-30 13:33:21 -0400

    test(integration): update snapshot tests

--- src/index.integration.test.ts
+++ src/index.integration.test.ts
@@ -24,107 +24,39 @@ describe('Paccurate', () => {
   it('sends post request to default endpoint', async () => {
     const paccurate = new Paccurate(PACCURATE_API_KEY)
     const data = await paccurate.pack(body)
-    expect(data).toMatchInlineSnapshot(
-      {
-        built: expect.any(String),
-        boxes: expect.any(Array),
-        packTime: expect.any(Number),
-        packUuid: expect.any(String),
-        renderTime: expect.any(Number),
-        requestFingerprint: expect.any(String),
-        responseFingerprint: expect.any(String),
-        startedAt: expect.any(String),
-        svgs: expect.any(Array),
-        totalTime: expect.any(Number),
-        usedKeyStem: expect.any(String),
-        version: expect.any(String),
-      },
-      `
-        {
-          "boxTypeChoiceGoalUsed": "lowest-cost",
-          "boxes": Any<Array>,
-          "built": Any<String>,
-          "host": "api.paccurate.io",
-          "itemSortReverseUsed": false,
-          "itemSortUsed": "none",
-          "leftovers": [],
-          "lenBoxes": 1,
-          "lenItems": 3,
-          "lenLeftovers": 0,
-          "lenUnits": 3,
-          "packTime": Any<Number>,
-          "packUuid": Any<String>,
-          "renderTime": Any<Number>,
-          "requestFingerprint": Any<String>,
-          "responseFingerprint": Any<String>,
-          "startedAt": Any<String>,
-          "svgs": Any<Array>,
-          "title": "Default",
-          "totalCost": 2550,
-          "totalTime": Any<Number>,
-          "totalVolume": 1334.8125,
-          "totalVolumeNet": 594,
-          "totalVolumeUsed": 594,
-          "totalVolumeUtilization": 0.44500632,
-          "totalWeight": 13.5,
-          "usedKeyStem": Any<String>,
-          "version": Any<String>,
-        }
-      `,
-    )
+    expect(data).toMatchSnapshot({
+      built: expect.any(String),
+      boxes: expect.any(Array),
+      packTime: expect.any(Number),
+      packUuid: expect.any(String),
+      renderTime: expect.any(Number),
+      requestFingerprint: expect.any(String),
+      responseFingerprint: expect.any(String),
+      startedAt: expect.any(String),
+      svgs: expect.any(Array),
+      totalTime: expect.any(Number),
+      usedKeyStem: expect.any(String),
+      version: expect.any(String),
+    })
   })
 
   it('sends post request to cloud endpoint', async () => {
     const paccurate = new Paccurate(PACCURATE_API_KEY, 'https://cloud.api.paccurate.io/')
     const data = await paccurate.pack(body)
-    expect(data).toMatchInlineSnapshot(
-      {
-        built: expect.any(String),
-        boxes: expect.any(Array),
-        packTime: expect.any(Number),
-        packUuid: expect.any(String),
-        renderTime: expect.any(Number),
-        requestFingerprint: expect.any(String),
-        responseFingerprint: expect.any(String),
-        startedAt: expect.any(String),
-        svgs: expect.any(Array),
-        totalTime: expect.any(Number),
-        usedKeyStem: expect.any(String),
-        version: expect.any(String),
-      },
-      `
-        {
-          "boxTypeChoiceGoalUsed": "lowest-cost",
-          "boxes": Any<Array>,
-          "built": Any<String>,
-          "host": "cloud.api.paccurate.io",
-          "itemSortReverseUsed": false,
-          "itemSortUsed": "none",
-          "leftovers": [],
-          "lenBoxes": 1,
-          "lenItems": 3,
-          "lenLeftovers": 0,
-          "lenUnits": 3,
-          "packTime": Any<Number>,
-          "packUuid": Any<String>,
-          "renderTime": Any<Number>,
-          "requestFingerprint": Any<String>,
-          "responseFingerprint": Any<String>,
-          "startedAt": Any<String>,
-          "svgs": Any<Array>,
-          "title": "Default",
-          "totalCost": 2550,
-          "totalTime": Any<Number>,
-          "totalVolume": 1334.8125,
-          "totalVolumeNet": 594,
-          "totalVolumeUsed": 594,
-          "totalVolumeUtilization": 0.44500632,
-          "totalWeight": 13.5,
-          "usedKeyStem": Any<String>,
-          "version": Any<String>,
-        }
-      `,
-    )
+    expect(data).toMatchSnapshot({
+      built: expect.any(String),
+      boxes: expect.any(Array),
+      packTime: expect.any(Number),
+      packUuid: expect.any(String),
+      renderTime: expect.any(Number),
+      requestFingerprint: expect.any(String),
+      responseFingerprint: expect.any(String),
+      startedAt: expect.any(String),
+      svgs: expect.any(Array),
+      totalTime: expect.any(Number),
+      usedKeyStem: expect.any(String),
+      version: expect.any(String),
+    })
   })
 
   test('responds with error if body is empty', async () => {
@@ -138,106 +70,38 @@ describe('Paccurate', () => {
 describe('pack', () => {
   it('sends post request to default endpoint', async () => {
     const data = await pack({ ...body, key: PACCURATE_API_KEY })
-    expect(data).toMatchInlineSnapshot(
-      {
-        built: expect.any(String),
-        boxes: expect.any(Array),
-        packTime: expect.any(Number),
-        packUuid: expect.any(String),
-        renderTime: expect.any(Number),
-        requestFingerprint: expect.any(String),
-        responseFingerprint: expect.any(String),
-        startedAt: expect.any(String),
-        svgs: expect.any(Array),
-        totalTime: expect.any(Number),
-        usedKeyStem: expect.any(String),
-        version: expect.any(String),
-      },
-      `
-        {
-          "boxTypeChoiceGoalUsed": "lowest-cost",
-          "boxes": Any<Array>,
-          "built": Any<String>,
-          "host": "api.paccurate.io",
-          "itemSortReverseUsed": false,
-          "itemSortUsed": "none",
-          "leftovers": [],
-          "lenBoxes": 1,
-          "lenItems": 3,
-          "lenLeftovers": 0,
-          "lenUnits": 3,
-          "packTime": Any<Number>,
-          "packUuid": Any<String>,
-          "renderTime": Any<Number>,
-          "requestFingerprint": Any<String>,
-          "responseFingerprint": Any<String>,
-          "startedAt": Any<String>,
-          "svgs": Any<Array>,
-          "title": "Default",
-          "totalCost": 2550,
-          "totalTime": Any<Number>,
-          "totalVolume": 1334.8125,
-          "totalVolumeNet": 594,
-          "totalVolumeUsed": 594,
-          "totalVolumeUtilization": 0.44500632,
-          "totalWeight": 13.5,
-          "usedKeyStem": Any<String>,
-          "version": Any<String>,
-        }
-      `,
-    )
+    expect(data).toMatchSnapshot({
+      built: expect.any(String),
+      boxes: expect.any(Array),
+      packTime: expect.any(Number),
+      packUuid: expect.any(String),
+      renderTime: expect.any(Number),
+      requestFingerprint: expect.any(String),
+      responseFingerprint: expect.any(String),
+      startedAt: expect.any(String),
+      svgs: expect.any(Array),
+      totalTime: expect.any(Number),
+      usedKeyStem: expect.any(String),
+      version: expect.any(String),
+    })
   })
 
   it('sends post request to cloud endpoint', async () => {
     const data = await pack({ ...body, key: PACCURATE_API_KEY }, 'https://cloud.api.paccurate.io/')
-    expect(data).toMatchInlineSnapshot(
-      {
-        built: expect.any(String),
-        boxes: expect.any(Array),
-        packTime: expect.any(Number),
-        packUuid: expect.any(String),
-        renderTime: expect.any(Number),
-        requestFingerprint: expect.any(String),
-        responseFingerprint: expect.any(String),
-        startedAt: expect.any(String),
-        svgs: expect.any(Array),
-        totalTime: expect.any(Number),
-        usedKeyStem: expect.any(String),
-        version: expect.any(String),
-      },
-      `
-        {
-          "boxTypeChoiceGoalUsed": "lowest-cost",
-          "boxes": Any<Array>,
-          "built": Any<String>,
-          "host": "cloud.api.paccurate.io",
-          "itemSortReverseUsed": false,
-          "itemSortUsed": "none",
-          "leftovers": [],
-          "lenBoxes": 1,
-          "lenItems": 3,
-          "lenLeftovers": 0,
-          "lenUnits": 3,
-          "packTime": Any<Number>,
-          "packUuid": Any<String>,
-          "renderTime": Any<Number>,
-          "requestFingerprint": Any<String>,
-          "responseFingerprint": Any<String>,
-          "startedAt": Any<String>,
-          "svgs": Any<Array>,
-          "title": "Default",
-          "totalCost": 2550,
-          "totalTime": Any<Number>,
-          "totalVolume": 1334.8125,
-          "totalVolumeNet": 594,
-          "totalVolumeUsed": 594,
-          "totalVolumeUtilization": 0.44500632,
-          "totalWeight": 13.5,
-          "usedKeyStem": Any<String>,
-          "version": Any<String>,
-        }
-      `,
-    )
+    expect(data).toMatchSnapshot({
+      built: expect.any(String),
+      boxes: expect.any(Array),
+      packTime: expect.any(Number),
+      packUuid: expect.any(String),
+      renderTime: expect.any(Number),
+      requestFingerprint: expect.any(String),
+      responseFingerprint: expect.any(String),
+      startedAt: expect.any(String),
+      svgs: expect.any(Array),
+      totalTime: expect.any(Number),
+      usedKeyStem: expect.any(String),
+      version: expect.any(String),
+    })
   })
 
   test('responds with error if body is empty', async () => {
